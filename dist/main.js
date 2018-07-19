87f0ffbe301affc323afa91449db0bf6f972f2e1
tristonwilliams <triston.williams@salesforce.com>
2018-07-19 13:09:44 +1000

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -72,7 +72,7 @@ __webpack_require__(1);
 var SDK = __webpack_require__(19);
 var sdk = new SDK();
 
-var address, width, height, link, color, mapsKey;
+var address, height, link, color, mapsKey;
 
 function debounce (func, wait, immediate) {
 	var timeout;
@@ -92,14 +92,13 @@ function debounce (func, wait, immediate) {
 function paintSettings () {
 	document.getElementById('text-input-id-0').value = mapsKey;
 	document.getElementById('text-input-id-1').value = address;
-	document.getElementById('slider-id-01').value = width;
 	document.getElementById('slider-id-02').value = height;
 	document.getElementById('text-input-id-2').value = link;
 	document.getElementById('drop-id-0').value = color;
 } 
 
 function paintSliderValues () {
-	document.getElementById('slider-id-01-val').innerHTML = document.getElementById('slider-id-01').value;
+	
 	document.getElementById('slider-id-02-val').innerHTML = document.getElementById('slider-id-02').value;
 	
 }
@@ -107,20 +106,18 @@ function paintSliderValues () {
 function paintMap() {
 	mapsKey = document.getElementById('text-input-id-0').value;
 	address = document.getElementById('text-input-id-1').value;
-	width = document.getElementById('slider-id-01').value;
 	height = document.getElementById('slider-id-02').value;
 	color = document.getElementById('drop-id-0').value
 	link = document.getElementById('text-input-id-2').value;
 	if (!address) {
 		return;
 	}
-	var url = "<table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td style='background-repeat:no-repeat;background-position: center;' background='" + mapsKey + "' bgcolor='#ffffff' width='" + width + "' height='" + height + "' valign='top'> <!--[if gte mso 9]> <v:rect xmlns:v='urn:schemas-microsoft-com:vml' fill='true' stroke='false' style='background-repeat:no-repeat; width:" + width + "px;height:" + height + "px;'> <v:fill type='tile' src='" + mapsKey + "' color='#ffffff' /> <v:textbox inset='0,0,0,0'> <![endif]--> <div> <table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td width='30' align='left' valign='top' style='font-size: 0%;' class='mobile-hidden'></td> <td align='left' valign='top'><table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td align='left' valign='top' style='padding-top: 95px;color:" + color + "; padding-left: 100px; padding-right: 100px; font-size: 48px;line-height: normal;'><a style='color: " + color + ";font-size: 48px;line-height: normal;Text-decoration: none;' href='" + link + "'><center>" + address + "</center></a></td> </tr> </tbody> </table></td> <td width='30' align='left' valign='top' style='font-size: 0%;'></td> </tr> </tbody> </table> </div> <!--[if gte mso 9]> </v:textbox> </v:rect> <![endif]--> </td> </tr> </tbody> </table>";
+	var url = "<table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td style='background-repeat:no-repeat;background-position: center;' background='" + mapsKey + "' bgcolor='#ffffff' width='600' height='" + height + "' valign='top'> <!--[if gte mso 9]> <v:rect xmlns:v='urn:schemas-microsoft-com:vml' fill='true' stroke='false' style='background-repeat:no-repeat; width:600px;height:" + height + "px;'> <v:fill type='tile' src='" + mapsKey + "' color='#ffffff' /> <v:textbox inset='0,0,0,0'> <![endif]--> <div> <table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td width='30' align='left' valign='top' style='font-size: 0%;' class='mobile-hidden'></td> <td align='left' valign='top'><table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td align='left' valign='top' style='padding-top: 95px;color:" + color + "; padding-left: 100px; padding-right: 100px; font-size: 48px;line-height: normal;'><a style='color: " + color + ";font-size: 48px;line-height: normal;Text-decoration: none;' href='" + link + "'><center>" + address + "</center></a></td> </tr> </tbody> </table></td> <td width='30' align='left' valign='top' style='font-size: 0%;'></td> </tr> </tbody> </table> </div> <!--[if gte mso 9]> </v:textbox> </v:rect> <![endif]--> </td> </tr> </tbody> </table>";
 	
 	
 	sdk.setContent(url);
 	sdk.setData({
 		address: address,
-		width: width,
 		height: height,
 		color: color,
 		link: link,
@@ -131,10 +128,9 @@ function paintMap() {
 
 sdk.getData(function (data) {
 	address = data.address || '';
-	width = data.width || 600;
 	height = data.height || 400;
 	link = data.link || '';
-	color = data.color || '#000000;';
+	color = data.color || 'Black';
 	mapsKey = data.mapsKey || '';
 	paintSettings();
 	paintSliderValues();
