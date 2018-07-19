c8985c8f176378e5765cc8de8b379e59bbb4178e
tristonwilliams <triston.williams@salesforce.com>
2018-07-19 13:34:42 +1000

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -128,9 +128,9 @@ function paintMap() {
 
 sdk.getData(function (data) {
 	address = data.address || 'Text Over Image';
-	height = data.height || 400;
+	height = data.height || 300;
 	link = data.link || '';
-	color = data.color || '#ffffff';
+	color = data.color || '#000000';
 	mapsKey = data.mapsKey || 'https://image.s4.exct.net/lib/fe911573736c007d7d/m/2/f3bcb75d-176b-4412-83f3-e70dc1c591c8.png';
 	paintSettings();
 	paintSliderValues();
