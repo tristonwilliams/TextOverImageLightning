74def95e8fb24a2591be1431fe13b7a5020d18b9
tristonwilliams <triston.williams@salesforce.com>
2018-07-19 12:28:47 +1000

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -113,7 +113,7 @@ function paintMap() {
 	if (!address) {
 		return;
 	}
-	var url = "<table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td style='background-repeat:no-repeat;background-position: center;' background='" + mapsKey + "' bgcolor='#ffffff' width='" + width + "' height='" + height + "' valign='top'> <!--[if gte mso 9]> <v:rect xmlns:v='urn:schemas-microsoft-com:vml' fill='true' stroke='false' style='background-repeat:no-repeat; width:" + width + "px;height:" + height + "px;'> <v:fill type='tile' src='" + mapsKey + "' color='#ffffff' /> <v:textbox inset='0,0,0,0'> <![endif]--> <div> <table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td width='30' align='left' valign='top' style='font-size: 0%;' class='mobile-hidden'></td> <td align='left' valign='top'><table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td align='left' valign='top' style='padding-top: 95px;color:#000000; padding-left: 100px; padding-right: 100px; font-size: 48px;'><a style='color: #000000;font-size: 48px;Text-decoration: none;' href='" + link + "'><center>" + address + "</center></a></td> </tr> </tbody> </table></td> <td width='30' align='left' valign='top' style='font-size: 0%;'></td> </tr> </tbody> </table> </div> <!--[if gte mso 9]> </v:textbox> </v:rect> <![endif]--> </td> </tr> </tbody> </table>";
+	var url = "<table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td style='background-repeat:no-repeat;background-position: center;' background='" + mapsKey + "' bgcolor='#ffffff' width='" + width + "' height='" + height + "' valign='top'> <!--[if gte mso 9]> <v:rect xmlns:v='urn:schemas-microsoft-com:vml' fill='true' stroke='false' style='background-repeat:no-repeat; width:" + width + "px;height:" + height + "px;'> <v:fill type='tile' src='" + mapsKey + "' color='#ffffff' /> <v:textbox inset='0,0,0,0'> <![endif]--> <div> <table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td width='30' align='left' valign='top' style='font-size: 0%;' class='mobile-hidden'></td> <td align='left' valign='top'><table width='100%' border='0' cellspacing='0' cellpadding='0'> <tbody> <tr> <td align='left' valign='top' style='padding-top: 95px;color:#000000; padding-left: 100px; padding-right: 100px; font-size: 48px;line-height: normal;'><a style='color: #000000;font-size: 48px;line-height: normal;Text-decoration: none;' href='" + link + "'><center>" + address + "</center></a></td> </tr> </tbody> </table></td> <td width='30' align='left' valign='top' style='font-size: 0%;'></td> </tr> </tbody> </table> </div> <!--[if gte mso 9]> </v:textbox> </v:rect> <![endif]--> </td> </tr> </tbody> </table>";
 	
 	
 	sdk.setContent(url);
@@ -130,8 +130,8 @@ function paintMap() {
 
 sdk.getData(function (data) {
 	address = data.address || '';
-	width = data.width || 400;
-	height = data.height || 300;
+	width = data.width || 600;
+	height = data.height || 400;
 	
 	link = data.link || '';
 	mapsKey = data.mapsKey || '';//localStorage.getItem('googlemapsapikeyforblock');
